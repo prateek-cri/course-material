03e55b5ce8f62c6ab07add739c9db45c34391264
Prateek Garg <prateek.garg16@gmail.com>
2014-09-22 16:04:42 +0200

    solut 014

---
+++ exercices/014/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:54:29 2014
+
+@author: prateek
+"""
+
+print(str(245850922 / 78256779) + '\n'+str((1 + 5 ** (0.5)) / 2))
+# print((1 + 5 ** (0.5)) / 2)
